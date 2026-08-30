22fcc0e201f54f86015359dd96992a5c483107a2
yash-g-linux <yashonlinux@gmail.com>
2026-08-30 15:04:33 +0100

    qiskit 2

---
+++ qiskit_test2.py
@@ -0,0 +1,28 @@
+from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator
+
+# Create a circuit with 2 qubits and 2 classical bits
+qc = QuantumCircuit(2, 2)
+
+# Put qubit 0 into superposition
+qc.h(0)
+
+# Entangle qubit 0 and qubit 1
+qc.cx(0, 1)
+
+# Measure both qubits
+qc.measure([0, 1], [0, 1])
+
+print("Quantum Circuit:")
+print(qc)
+
+# Simulate the circuit
+simulator = AerSimulator()
+job = simulator.run(qc, shots=1000)
+result = job.result()
+
+# Get measurement counts
+counts = result.get_counts()
+
+print("\nMeasurement Results:")
+print(counts)
