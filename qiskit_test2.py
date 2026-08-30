eb85fac2c6697026f930b02b17606fcbeac641e9
yash-g-linux <yashonlinux@gmail.com>
2026-08-30 15:50:07 +0100

    Clarity on measurement

--- qiskit_test2.py
+++ qiskit_test2.py
@@ -10,15 +10,18 @@ qc.h(0)
 # Entangle qubit 0 and qubit 1
 qc.cx(0, 1)
 
+qc.x(0)
+
 # Measure both qubits
-qc.measure([1, 1], [1, 1])
+qc.measure([0,1], [0,1])
+#qc.measure(qubit numbers array, cbit numbers array)
 
 print("Quantum Circuit:")
 print(qc)
 
 # Simulate the circuit
 simulator = AerSimulator()
-job = simulator.run(qc, shots=1000)
+job = simulator.run(qc, shots=10000)
 result = job.result()
 
 # Get measurement counts
